60337164618b7ab7db0f478f32acdd4b9ab3a95e
prathutiwari <prathutiwari22@gmail.com>
2025-03-02 02:42:09 +0530

    first commit

---
+++ src/app/layout/navigation/navigation.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnInit {
+
+  urlActive:any;
+
+  constructor(private router: Router,
+  ) { 
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  reloadPage() {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    const currentUrl = this.router.url + '?';
+    this.router.navigateByUrl(currentUrl)
+      .then(() => {
+        this.router.navigated = false;
+        this.router.navigate([this.router.url]);
+      });
+      console.log('reload')
+  }
+
+}
